55e55b22dab09b4b59132952e8533bc1a98a15ee
Nirasha <demelwgnd.22@uom.lk>
2025-11-30 23:44:28 +0530

    Feat: enhance Activity model with additional fields and annotations for metrics tracking

--- activityservice/activityservice/src/main/java/com/example/activityservice/model/Activity.java
+++ activityservice/activityservice/src/main/java/com/example/activityservice/model/Activity.java
@@ -1,13 +1,26 @@
 package com.example.activityservice.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Document(collection = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
+    @Id
     private String id;
     private String userId;
     private ActivityType type;
     private Integer duration;
     private Integer caloriesBurned;
     private LocalDateTime startTime;
+    @Field("metrics")
     private Map<String, Object> additionalMetrics;
+    @CreatedDate
     private LocalDateTime getCreatedAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
 }
