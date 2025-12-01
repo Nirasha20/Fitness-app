613aa4e3385b890f25f62bc61380b856d65600e5
Nirasha <demelwgnd.22@uom.lk>
2025-12-01 22:59:59 +0530

    Feat: refactor Activity class to include necessary imports for annotations and data handling

--- activityservice/activityservice/src/main/java/com/example/activityservice/model/Activity.java
+++ activityservice/activityservice/src/main/java/com/example/activityservice/model/Activity.java
@@ -1,5 +1,15 @@
 package com.example.activityservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
