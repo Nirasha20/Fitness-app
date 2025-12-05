7c550711a11e9a990e4bb7896eea5b293e9ae9ab
Nirasha <demelwgnd.22@uom.lk>
2025-12-05 14:43:34 +0530

    Feat: add initial ActivityController with REST endpoint for activities

---
+++ activityservice/activityservice/src/main/java/com/example/activityservice/controller/ActivityContoller.java
@@ -0,0 +1,9 @@
+package com.example.activityservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;  
+@RestController
+@RequestMapping("/api/activities")
+public class ActivityContoller {
+
+}
